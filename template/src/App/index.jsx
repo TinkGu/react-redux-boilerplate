0b41363b2b8c7fe08a0262ad88dcbcaa52e410e1
tinkgu <tlwrite@126.com>
2017-12-14 19:01:23 +0800

    [hotfix] delete redux code in standard mode

--- template/src/App/index.jsx
+++ template/src/App/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 {{#redux}}
+import { Provider } from 'react-redux'
 import store from './store'
 {{/redux}}
 import Routers from './Routers'
